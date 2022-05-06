57bde45104bdf06940007aa6d8f94e9093af9f8d
mauriciobarroso <mauriciobarroso1990@gmail.com>
2022-05-06 00:12:56 -0400

    added octave files for process measurements

---
+++ octave/measurement1.m
@@ -0,0 +1,36 @@
+%%
+% Import CSV file and plot timestamp vs acce_z
+%%
+csv_file = csvread('A1651795708816.csv');
+timestamp = csv_file(:, 1);
+acce_z = csv_file(:, 2);
+figure;
+plot(timestamp, acce_z)
+
+%%
+% Delete first element (zero)
+%%
+acce_z = acce_z(2:end);
+
+%%
+% Calculate min and max values to filter data
+%%
+acce_z_min = min(acce_z)
+acce_z_max = max(acce_z)
+
+acce_z = min(acce_z, acce_z_max - 0.01);
+acce_z = max(acce_z, acce_z_min + 0.01);
+
+%%
+% Histogram with 10 bins and 50 bins
+%%
+figure;
+hist(acce_z, 10)
+figure;
+hist(acce_z, 50)
+
+%%
+% Mean and variance
+%%
+acce_z_mean = mean(acce_z)
+acce_z_var = var(acce_z)
---
+++ octave/measurement3.m
@@ -0,0 +1,28 @@
+pkg load signal
+
+%%
+% Import CSV file and plot timestamp vs acce_z
+%%
+csv_file = csvread('A1651796182317.csv');
+timestamp = csv_file(:, 1);
+acce_z = csv_file(:, 2);
+figure;
+plot(timestamp, acce_z)
+
+%%
+% Delete mean value
+%%
+%accez = (acce_z - mean(acce_z.*window)/mean(window)).*window;
+acce_z_rmean = detrend(acce_z, 'constant');
+
+
+%%
+% Plot spectrogram
+%%
+fs = 100 % Sample rate in Hz
+window = ceil(3 / (1 / fs)) % Window in samples
+overlap = ceil(2.8 / (1 / fs)) % Overlap in samples
+
+figure;
+specgram(acce_z_rmean, 1024, fs, window, overlap)
+colormap('jet')
